b204a2cce2134910aaf322579db03489b9ca53e7
David Sherret <dsherret@gmail.com>
2020-06-27 14:57:11 -0400

    Add replace-plugin.js script.

---
+++ scripts/replace-plugin.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const [pluginName, version, fileName] = process.argv.slice(2);
+
+const infoText = fs.readFileSync("info.json", { encoding: "utf8" });
+const info = JSON.parse(infoText);
+
+const plugin = info.latest.find(plugin => plugin.name === pluginName);
+if (plugin == null) {
+    throw new Error(`Could not find plugin: ${pluginName}`);
+}
+
+plugin.version = version;
+plugin.url = `https://plugins.dprint.dev/${fileName}.wasm`;
+
+fs.writeFileSync("info.json", JSON.stringify(info, undefined, 2) + "\n");
