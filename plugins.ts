c2ca598565c676ebb7914378fce820d34aea0a0f
David Sherret <dsherret@gmail.com>
2021-04-18 20:27:46 -0400

    feat: #1 - Show latest plugins on plugins.dprint.dev.

---
+++ plugins.ts
@@ -0,0 +1,73 @@
+import { parseVersion } from "./version.ts";
+
+export interface PluginResolver {
+    tryGetVersion(url: URL): string | undefined;
+    getRedirectUrl(version: string): string;
+}
+
+export const pluginResolvers: PluginResolver[] = [{
+    tryGetVersion(url) {
+        return /^\/typescript-([0-9]+\.[0-9]+\.[0-9]+).wasm$/.exec(url.pathname)?.[1];
+    },
+    getRedirectUrl(version) {
+        if (parseVersion(version).lessThanEqual(parseVersion("0.44.0"))) {
+            return `https://github.com/dprint/dprint-plugin-typescript/releases/download/${version}/typescript-${version}.wasm`;
+        } else {
+            return `https://github.com/dprint/dprint-plugin-typescript/releases/download/${version}/typescript.wasm`;
+        }
+    },
+}, {
+    tryGetVersion(url) {
+        return /^\/json-([0-9]+\.[0-9]+\.[0-9]+).wasm$/.exec(url.pathname)?.[1];
+    },
+    getRedirectUrl(version) {
+        if (parseVersion(version).lessThanEqual(parseVersion("0.10.1"))) {
+            return `https://github.com/dprint/dprint-plugin-json/releases/download/${version}/json-${version}.wasm`;
+        } else {
+            return `https://github.com/dprint/dprint-plugin-json/releases/download/${version}/json.wasm`;
+        }
+    },
+}, {
+    tryGetVersion(url) {
+        return /^\/markdown-([0-9]+\.[0-9]+\.[0-9]+).wasm$/.exec(url.pathname)?.[1];
+    },
+    getRedirectUrl(version) {
+        if (parseVersion(version).lessThanEqual(parseVersion("0.7.0"))) {
+            return `https://github.com/dprint/dprint-plugin-markdown/releases/download/${version}/markdown-${version}.wasm`;
+        } else {
+            return `https://github.com/dprint/dprint-plugin-markdown/releases/download/${version}/markdown.wasm`;
+        }
+    },
+}, {
+    tryGetVersion(url) {
+        return /^\/rustfmt-([0-9]+\.[0-9]+\.[0-9]+).wasm$/.exec(url.pathname)?.[1];
+    },
+    getRedirectUrl(version) {
+        if (parseVersion(version).lessThanEqual(parseVersion("0.3.0"))) {
+            return `https://github.com/dprint/dprint-plugin-rustfmt/releases/download/${version}/rustfmt-${version}.wasm`;
+        } else {
+            return `https://github.com/dprint/dprint-plugin-rustfmt/releases/download/${version}/rustfmt.wasm`;
+        }
+    },
+}, {
+    tryGetVersion(url) {
+        return /^\/prettier-([0-9]+\.[0-9]+\.[0-9]+).exe-plugin$/.exec(url.pathname)?.[1];
+    },
+    getRedirectUrl(version) {
+        return `https://github.com/dprint/dprint-plugin-prettier/releases/download/${version}/prettier.exe-plugin`;
+    },
+}, {
+    tryGetVersion(url) {
+        return /^\/roslyn-([0-9]+\.[0-9]+\.[0-9]+).exe-plugin$/.exec(url.pathname)?.[1];
+    },
+    getRedirectUrl(version) {
+        return `https://github.com/dprint/dprint-plugin-roslyn/releases/download/${version}/roslyn.exe-plugin`;
+    },
+}, {
+    tryGetVersion(url) {
+        return /^\/yapf-([0-9]+\.[0-9]+\.[0-9]+).exe-plugin$/.exec(url.pathname)?.[1];
+    },
+    getRedirectUrl(version) {
+        return `https://github.com/dprint/dprint-plugin-yapf/releases/download/${version}/yapf.exe-plugin`;
+    },
+}];
