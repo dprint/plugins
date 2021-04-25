6548512fa2d723fcec5fe1931c4e42e9cb7ce41e
David Sherret <dsherret@gmail.com>
2021-04-25 14:01:58 -0400

    feat: Add `/download/pluginname-x.x.x.wasm` route for downloading from the playground
    This was added due to CORS issues.

--- mod.ts
+++ mod.ts
@@ -8,11 +8,14 @@ addEventListener("fetch", event => {
 function handleRequest(request: Request) {
     const url = new URL(request.url);
 
-    for (const plugin of pluginResolvers) {
-        const version = plugin.tryGetVersion(url);
-        if (version != null) {
-            return createRedirectResponse(plugin.getRedirectUrl(version));
-        }
+    const pluginUrl = tryResolvePluginUrl(url);
+    if (pluginUrl != null) {
+        return createRedirectResponse(pluginUrl);
+    }
+
+    // match route starting with /downloads/plugin-x.x.x.wasm which will serve the response from this server
+    if (url.pathname.startsWith("/download/") && url.pathname.endsWith(".wasm")) {
+        return resolveRedirectDownloadResponse(new URL(request.url.replace(/\/download\//, "/")));
     }
 
     if (url.pathname.startsWith("/info.json")) {
@@ -50,13 +53,52 @@ function handleRequest(request: Request) {
     });
 }
 
+// needed this to get the playground working due to CORs on GitHub
+function resolveRedirectDownloadResponse(requestUrl: URL) {
+    const pluginUrl = tryResolvePluginUrl(requestUrl);
+    return pluginUrl == null ? create404Response() : createWasmDownloadRedirectResponse(pluginUrl);
+
+    function createWasmDownloadRedirectResponse(location: string) {
+        return fetch(location)
+            .then(response => {
+                if (response.status !== 200) {
+                    return response;
+                }
+
+                return new Response(response.body, {
+                    headers: {
+                        "content-type": "application/wasm",
+                        // allow the playground to download this
+                        "Access-Control-Allow-Origin": "https://dprint.dev",
+                    },
+                    status: 200,
+                });
+            });
+    }
+}
+
+function tryResolvePluginUrl(url: URL) {
+    for (const plugin of pluginResolvers) {
+        const version = plugin.tryGetVersion(url);
+        if (version != null) {
+            return plugin.getRedirectUrl(version);
+        }
+    }
+    return undefined;
+}
+
 function createRedirectResponse(location: string) {
     return new Response(null, {
         headers: {
             location,
-            "Access-Control-Allow-Origin": "*",
-            "Referrer-Policy": "no-referrer",
         },
         status: 302,
+    });
+}
+
+function create404Response() {
+    return new Response(null, {
+        status: 404,
+        statusText: "Not Found",
     });
 }
