2e4a76dd11ff49b23645c93fad4b27e8acd77533
David Sherret <dsherret@gmail.com>
2023-11-18 17:52:18 -0500

    refactor: add import map

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-import { serve } from "https://deno.land/std@0.122.0/http/server.ts";
 import { handleRequest } from "./handleRequest.ts";
 
-await serve((request) => handleRequest(request));
+Deno.serve((request) => handleRequest(request));
